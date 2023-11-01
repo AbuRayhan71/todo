8dd664d1afd2afd65fb3bb0b3c76ca10c309349e
AbuRayhan71 <86045592+Rayhan7111@users.noreply.github.com>
2023-11-02 07:20:42 +1100

    more clean v2

--- app/src/main/java/com/example/todolist/data/model/TaskInfo.kt
+++ app/src/main/java/com/example/todolist/data/model/TaskInfo.kt
@@ -2,10 +2,12 @@ package com.example.todolist.data.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.Embedded
+import androidx.room.Relation
 import java.io.Serializable
-import java.sql.Time
-import java.util.*
+import java.util.Date
 
+// Defines the structure for a task entity with various details.
 @Entity(tableName = "taskInfo")
 data class TaskInfo(
     @PrimaryKey(autoGenerate = false)
@@ -17,3 +19,12 @@ data class TaskInfo(
     var category: String
 ) : Serializable
 
+// Represents the relationship between a task and its category.
+data class TaskCategoryInfo(
+    @Embedded val taskInfo: TaskInfo, // The task details are embedded within this class.
+    @Relation(
+        parentColumn = "category", // The column in TaskInfo that relates to CategoryInfo.
+        entityColumn = "categoryInformation" // The column in CategoryInfo that TaskInfo refers to.
+    )
+    val categoryInfo: List<CategoryInfo> // A list of categories associated with the task.
+) : Serializable
