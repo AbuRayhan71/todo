7892663a42d450d02b066ae3e7f6b5265fcf95d7
AbuRayhan71 <86045592+Rayhan7111@users.noreply.github.com>
2023-11-02 05:32:53 +1100

    comm

--- app/src/main/java/com/example/todolist/data/db/DateConverter.kt
+++ app/src/main/java/com/example/todolist/data/db/DateConverter.kt
@@ -4,8 +4,11 @@ import androidx.room.TypeConverter
 import java.util.*
 
 class DateConverter {
+    // Converts a timestamp (long number) to a Date object.
     @TypeConverter
      fun fromTimeStamp(value : Long) : Date = Date(value)
     @TypeConverter
+    // Converts a Date object to a timestamp (long number)
     fun dateToTimeStamp(date : Date) : Long = date.time
 }
+//
